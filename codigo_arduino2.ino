a51a9b43aa7bacb1356d66c87360485eb4507234
Arthur Nunes <122186734+tutukkj@users.noreply.github.com>
2025-08-27 09:00:24 -0300

    Codigo Arduino

---
+++ codigo_arduino2.ino
@@ -0,0 +1,29 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 13, 7, 6, 5, 4);
+
+String ultimoTexto = "";
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Aguardando...");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String texto = Serial.readStringUntil('\n');
+    texto.trim(); // Remove espaços e quebras extras
+
+    if (texto.length() > 0 && texto != ultimoTexto) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Pilhas:");
+      lcd.setCursor(0, 1);
+      lcd.print(texto);
+      ultimoTexto = texto;
+    }
+  }
+}
